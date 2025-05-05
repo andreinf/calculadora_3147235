0fe389fda650a3cbf7af512684f5d8a2fbed14d0
Luyer <luyerperez0@gmail.com>
2025-05-05 15:46:34 -0500

    /, ** y //

--- operaciones/operaciones.py
+++ operaciones/operaciones.py
@@ -6,3 +6,24 @@ def restar (a, b):
 
 def multiplicar (a, b):
     return a * b
+# operaciones.
+#def se usa para definir funciones
+# y abajo se retorna una operacion o funcion de tenga esta def
+#valueError es un comando donde un texto se muestra como error
+ # operaciones.py
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
+
+def potencia(a, b):
+    try:
+        return a ** b  # Intentamos realizar la operación de potencia
+    except OverflowError:
+        raise ValueError("El resultado es demasiado grande para manejarlo")  # Excepción si el resultado es demasiado grande
+
+def division_entera(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a // b
