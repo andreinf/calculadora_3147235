6382a1d479dbe9c74ed78f555c0d652cb4045606
DinaMalaver <monroydina880@gmail.com>
2025-05-05 14:44:46 -0500

    operaciones +, - y *

---
+++ prueba.py
@@ -0,0 +1,7 @@
+from operaciones import operaciones
+
+print("suma 3 + 5 =", operaciones.sumar(3,5))
+
+print("resta 10 - 4 =", operaciones.restar(10,4))
+
+print("multiplica 3 * 8 =", operaciones.multiplicar(3,8))
